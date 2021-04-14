dfd8be82a26f69f603f49b4b22b63c6ade96b75c
Nicolò Albergoni <nicolo.albe@gmail.com>
2021-04-14 23:46:01 +0200

    Finish A09 Primitives

--- A09 - Primitives/models.js
+++ A09 - Primitives/models.js
@@ -9,15 +9,32 @@ function buildGeometry() {
 
 
 	// Draws a filled S-shaped pattern (replace the vertices and primitive type)
-	var vert2 = [[-1.0,-1.0,-1.0], [1.0,-1.0,-1.0], [1.0, 1.0,-1.0], ];
+	var vert2 = [[-4,-7,0], [-4,-5,0], [1,-7,0], [-1,-5,0], [1,1,0], [-1,-1,0],
+							[-2, 1,0], [-4,-1,0], [-2,5,0], [-4,7,0], [1,5,0], [1,7,0]];							
 
-	addMesh(vert2, "F", [0.0, 0.0, 1.0]);
+	addMesh(vert2, "S", [0.0, 0.0, 1.0]);
 
 
 	// Draws a filled pentacong (replace the vertices and primitive type)
-	var vert3 = [[-1.0,-1.0, 1.0], [1.0,-1.0, 1.0], [1.0,1.0, 1.0], [-1.0,1.0, 1.0], [0.0,1.5, 1.0], [2.0, 1.0, 0.0]];
+	let r = 2; // Radius of the inscribed circumference
+	let tr = 2; // Length from vertex to center for translation
 
-	addMesh(vert3, "T", [0.0, 1.0, 0.0]);
+
+	//Projection of translation vector
+	let tr_x = tr * Math.cos(utils.degToRad(54));
+	let tr_y = tr * Math.sin(utils.degToRad(54));
+
+	let va = [tr_x + (r * Math.cos(utils.degToRad(18))), tr_y + (r * Math.sin(utils.degToRad(18))), 0];
+	let vb = [0 + tr_x, tr_y + r, 0];
+	let vc = [tr_x + (r * Math.cos(utils.degToRad(-54))), 0, 0];
+  let ve = [tr_x + (-r * Math.cos(utils.degToRad(18))) , tr_y + (r * Math.sin(utils.degToRad(18))) , 0];
+
+	let vert3 = [[0,0,0], ve, vb, va, vc];
+
+	// More "naive" solution
+	//var vert3 = [[0,0,0], [-1.5,4.5,0], [2,7,0], [5.5,4.5,0], [4,0,0]];
+
+	addMesh(vert3, "F", [0.0, 1.0, 0.0]);
 	
 }
 
